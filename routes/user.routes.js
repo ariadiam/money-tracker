41edc86dab1e7d399d8f6364e468180628003d4e
Ar. Diamanti <ariadiamanti@gmail.com>
2025-07-12 00:24:53 +0300

    feat: role verification added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 
 const userController = require('../controllers/user.controller');
 const verifyToken = require('../middlewares/auth.middleware').verifyToken
+const verifyRoles = require('../middlewares/auth.middleware').verifyRoles;
 
 router.get('/', userController.findAll);
 router.get('/:userId', userController.findOne);
-router.post('/', verifyToken, userController.create);
+router.post('/', verifyToken, verifyRoles("admin"), userController.create);
 router.patch('/:userId', userController.updateById);
 router.delete('/:userId', userController.deleteById);
 
