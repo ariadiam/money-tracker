362b52dae04423b3c3cab7effc26e3f13c239316
Ar. Diamanti <ariadiamanti@gmail.com>
2025-07-30 14:51:38 +0300

    feat: add cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger');
@@ -9,6 +11,10 @@ const swaggerDocument = require('./swagger');
 const userRoutes = require('./routes/user.routes');
 const transactionRoutes = require('./routes/transaction.routes'); 
 const authRoutes = require('./routes/auth.routes');
+
+app.use(cors({
+  origin: ['http://localhost:3000']
+}))
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);          
