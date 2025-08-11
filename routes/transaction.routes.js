755a3008b8e128d582c0ce93880eec6003a4e015
Ar. Diamanti <ariadiamanti@gmail.com>
2025-08-11 14:51:31 +0300

    feat: token verification added

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -2,21 +2,17 @@ const express = require('express');
 const router = express.Router();
 const transactionController = require('../controllers/transaction.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
+const { verifyToken } = require('../middlewares/auth.middleware');
 
-// Get all transactions for a user
-router.get('/:userId/transactions', transactionController.getUserTransactions);
+router.get('/:userId/transactions', verifyToken, transactionController.getUserTransactions);
 
-// Add a new transaction
-router.post('/:userId/transactions', transactionController.addTransaction);
+router.post('/:userId/transactions', verifyToken, transactionController.addTransaction);
 
-// Update a transaction
-router.patch('/:userId/transactions/:transactionId', transactionController.updateTransaction);
+router.patch('/:userId/transactions/:transactionId', verifyToken, transactionController.updateTransaction);
 
-// Delete a transaction
-router.delete('/:userId/transactions/:transactionId', transactionController.deleteTransaction);
+router.delete('/:userId/transactions/:transactionId', verifyToken, transactionController.deleteTransaction);
 
-// Get financial summary
-router.get('/:userId/transactions/summary', transactionController.getSummary);
+router.get('/:userId/transactions/summary', verifyToken, transactionController.getSummary);
 
 // router.get('/', authMiddleware, transactionController.getUserTransactions);
 
